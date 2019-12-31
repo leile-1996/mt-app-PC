09e8299fffc2f1588ef1ab5080d00ca4c2b5a7ab
leile <3299212724@qq.com>
2020-01-01 04:11:25 +0800

    2th-commit

--- src/components/products/crumbs.vue
+++ src/components/products/crumbs.vue
@@ -1,7 +1,7 @@
 <template>
   <el-breadcrumb separator-class="el-icon-arrow-right">
-    <el-breadcrumb-item>{{$store.state.position}}美团</el-breadcrumb-item>
-    <el-breadcrumb-item>{{$store.state.position}}火锅</el-breadcrumb-item>
+    <el-breadcrumb-item>美团</el-breadcrumb-item>
+    <el-breadcrumb-item>火锅</el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
